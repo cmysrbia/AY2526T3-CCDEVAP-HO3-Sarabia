13b074f36120d2a2c1c50dc02ec6b19ac99a2847
cmysrbia <erikaghirl@gmail.com>
2026-05-28 22:30:40 +0800

    added checkAnswer() and playAgain()

--- scripts.js
+++ scripts.js
@@ -1,6 +1,5 @@
-// global variables
-let score = 0;
-let currentAnswer = 0;
+let score = 0; // current score
+let answer = 0; // current answer
 const operations = ["+", "-", "*"];
 
 // generate question
@@ -20,16 +19,61 @@ function generateQuestion(){
     // compute answer
     switch (operation){
         case "+":
-            currentAnswer = num1 + num2;
+            answer = num1 + num2;
             break;
-
         case "-":
-            currentAnswer = num1 - num2;
+            answer = num1 - num2;
             break;
-
         case "*":
-            currentAnswer = num1 * num2;
+            answer = num1 * num2;
             break;
     }
+}
+
+// check answer
+function checkAnswer(){
+    let input = Number(document.getElementById("answer").value);
+    let message = document.getElementById("message");
+
+    if (input === answer){
+        score++;
+        message.innerHTML = "Correct!";
+        message.style.color = "green";
+    } else {
+        message.innerHTML = "Wrong! Correct answer is " + currentAnswer + ".";
+        message.style.color = "red";
+    }
+
+    // update score
+    document.getElementById("score").innerHTML = score;
+    // clear input
+    document.getElementById("answer").value = "";
+
+    // check win condition
+    if (score === 5){
+        document.getElementById("div-questions").style.display = "none";
+        document.getElementById("div-success").style.display = "block";
+    } else {
+        // generate next question
+        generateQuestion();
+    }
+}
+
+// play again
+function playAgain(){
+
+    // reset
+    score = 0;
+    document.getElementById("score").innerHTML = score;
+    // hide sucess message
+    document.getElementById("div-success").style.display = "none";
+    // show question section
+    document.getElementById("div-questions").style.display = "block";
+    // clear prev msg
+    document.getElementById("message").innerHTML = "";
+    // clear input
+    document.getElementById("answer").value = "";
+
+    generateQuestion();
 }
 
